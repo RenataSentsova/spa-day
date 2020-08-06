943d767c4cbba99f7fa450eac0ba992735961eaf
RenataSentsova <renatasentsova@gmail.com>
2020-08-06 03:36:39 -0400

    added validation annotations

--- src/main/java/org/launchcode/spaDay/controllers/UserController.java
+++ src/main/java/org/launchcode/spaDay/controllers/UserController.java
@@ -3,31 +3,31 @@ package org.launchcode.spaDay.controllers;
 import org.launchcode.spaDay.models.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
+
 @Controller
 @RequestMapping("user")
 public class UserController {
 
-    @GetMapping("add")
-    public String displayAddUserForm() {
+    @GetMapping("/add")
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
+        model.addAttribute("title", "Add a New User");
         return "user/add";
     }
 
-    @PostMapping("add")
-    public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        String name = "User " + user.getUsername();
-        model.addAttribute("name", name);
-        if(verify.equals(user.getPassword())) {
-            return "user/index";
-        } else {
-            model.addAttribute("error", "Password is not verified");
+    @PostMapping("")
+    public String processAddUserForm(@ModelAttribute @Valid User user, Errors errors, Model model) {
+        if (errors.hasErrors()) {
+            model.addAttribute("title", "Add a New User");
             return "user/add";
         }
+        return "user/index";
     }
- }
+}
--- src/main/java/org/launchcode/spaDay/models/User.java
+++ src/main/java/org/launchcode/spaDay/models/User.java
@@ -1,12 +1,38 @@
 package org.launchcode.spaDay.models;
 
-public class User {
-    private String username, email, password;
+import javax.validation.constraints.*;
 
-    public User(String username, String email, String password) {
+public class User {
+    @NotEmpty(message="Username required!")
+    @Size(min = 5, max = 15, message = "Username must be between 5 and 15 characters!")
+    private String username;
+
+    @Email
+    private String email;
+
+    @Min(value = 6, message = "Password must be at least 6 characters long!")
+    @NotNull(message="Password required!")
+    private String password;
+
+    @NotNull(message = "Passwords do not match!")
+    private String verifyPassword;
+
+    public User() { }
+
+    public User(String username, String email, String password, String verify, String verifyPassword) {
+        this();
         this.username = username;
         this.email = email;
         this.password = password;
+        this.verifyPassword = verifyPassword;
+    }
+
+    private void checkPassword() {
+        if(this.password != null && this.verifyPassword != null) {
+            if (!this.password.equals(verifyPassword)) {
+                this.verifyPassword = null;
+            }
+        }
     }
 
     public String getUsername() {
@@ -31,5 +57,15 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+        checkPassword();
+    }
+
+    public String getVerifyPassword() {
+        return verifyPassword;
+    }
+
+    public void setVerifyPassword(String verifyPassword) {
+        this.verifyPassword = verifyPassword;
+        checkPassword();
     }
 }
